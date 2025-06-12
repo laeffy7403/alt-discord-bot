1714a19e479672f37fc11e871a7b0d2ccf45e72a
LigitPizza <laeffy7403@gmail.com>
2025-06-12 23:36:12 +0800

    pusg

--- index.js
+++ index.js
@@ -20,22 +20,22 @@ const client = new Discord.Client({
     Discord.Intents.FLAGS.GUILD_MESSAGES
   ]
 });
-client.on("messageCreate", message => {
-  console.log("=== MESSAGE DETECTED ===");
-  console.log("Author:", message.author.username);
-  console.log("Content:", message.content);
-  console.log("Is bot:", message.author.bot);
-  console.log("========================");
+// client.on("messageCreate", message => {
+//   console.log("=== MESSAGE DETECTED ===");
+//   console.log("Author:", message.author.username);
+//   console.log("Content:", message.content);
+//   console.log("Is bot:", message.author.bot);
+//   console.log("========================");
   
-  if (message.author.bot) return;
+//   if (message.author.bot) return;
 
-  const msgContent = message.content.toLowerCase();
+//   const msgContent = message.content.toLowerCase();
   
-  var signal = ["-- wes --"];
-  if (signal.some(trigger => msgContent.includes(trigger))) {
-    message.channel.send("receive signal from wes!");
-  }
-})
+//   var signal = ["-- wes --"];
+//   if (signal.some(trigger => msgContent.includes(trigger))) {
+//     message.channel.send("receive signal from wes!");
+//   }
+// })
 
 
   client.once('ready', () => {
@@ -44,14 +44,14 @@ client.on("messageCreate", message => {
   const channelId = '1380085584394981428'; // change this
   // const interval = 10 * 60 * 1000; // 10 minutes in milliseconds
   // const interval = 5 * 60 * 1000; // 5 minutes in milliseconds
-  const interval = 15* 1000; // 5 sec in milliseconds
+  const interval = 15 * 1000; // 15 sec in milliseconds
   var i = 0;
 
   setInterval(() => {
     const channel = client.channels.cache.get(channelId);
     if (channel) {
       i++;
-      channel.send(`-- monika --`);
+      channel.send(`<@970283613818073098>`);
     } else {
       console.log('Channel not found.');
     }
